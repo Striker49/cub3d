3e75be8f87472b7dbbebe49eefd9b67b1ebfedf1
Sebastien Roy <seroy@c2r2p9.42quebec.com>
2024-03-15 15:36:20 -0400

    commented minimap

--- src/main.c
+++ src/main.c
@@ -3,60 +3,60 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void	draw_square(t_data *data, int32_t i, int32_t y, int32_t color)
-{
-	int	l;
-	int	k;
-	k = y;
-	l = i;
-	while (y < k + 9)
-	{
-		i = l ;
-		while (i < l + 9)
-		{
-			mlx_put_pixel(data->img[0], i, y, color);
-			i++;
-		}
-		y++;
-	}
-}
+// void	draw_square(t_data *data, int32_t i, int32_t y, int32_t color)
+// {
+// 	int	l;
+// 	int	k;
+// 	k = y;
+// 	l = i;
+// 	while (y < k + 9)
+// 	{
+// 		i = l ;
+// 		while (i < l + 9)
+// 		{
+// 			mlx_put_pixel(data->img[0], i, y, color);
+// 			i++;
+// 		}
+// 		y++;
+// 	}
+// }
 
-int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
-{
-    return (r << 24 | g << 16 | b << 8 | a);
-}
+// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
+// {
+//     return (r << 24 | g << 16 | b << 8 | a);
+// }
 
-void ft_put_pixel(void* param)
-{
-	t_data	*data;
-	uint32_t y;
-	uint32_t i;
-	uint32_t color;
+// void ft_put_pixel(void* param)
+// {
+// 	t_data	*data;
+// 	uint32_t y;
+// 	uint32_t i;
+// 	uint32_t color;
 
-	data = param;
-	y = 0;
-	while (y < data->height)
-	{
-		i = 0;
-		while (i < data->width)
-		{
-			// printf("i: %d\n", i);
-			if (data->map[y][i] == '0')
-				color = ft_pixel(0xFF, 0xFF, 0xFF,0xFF);
-			else if (data->map[y][i] == '1')
-				color = ft_pixel(0x0, 0x0, 0x8B,0xFF);
-			else if (data->map[y][i] == 'P')
-				color = ft_pixel(0xDC, 0x14, 0x3C,0xFF);
-			else
-				color = ft_pixel(0x0, 0x0, 0x0,0xFF);
-			draw_square(data, i * 10, y * 10, color);
-			// mlx_put_pixel(data->img[0], i * 10, y * 10, color);
-			i++;
-		}
-		// printf("y: %d\n", y);
-		y++;
-	}
-}
+// 	data = param;
+// 	y = 0;
+// 	while (y < data->height)
+// 	{
+// 		i = 0;
+// 		while (i < data->width)
+// 		{
+// 			// printf("i: %d\n", i);
+// 			if (data->map[y][i] == '0')
+// 				color = ft_pixel(0xFF, 0xFF, 0xFF,0xFF);
+// 			else if (data->map[y][i] == '1')
+// 				color = ft_pixel(0x0, 0x0, 0x8B,0xFF);
+// 			else if (data->map[y][i] == 'P')
+// 				color = ft_pixel(0xDC, 0x14, 0x3C,0xFF);
+// 			else
+// 				color = ft_pixel(0x0, 0x0, 0x0,0xFF);
+// 			draw_square(data, i * 10, y * 10, color);
+// 			// mlx_put_pixel(data->img[0], i * 10, y * 10, color);
+// 			i++;
+// 		}
+// 		// printf("y: %d\n", y);
+// 		y++;
+// 	}
+// }
 
 int	main( int argc, char **argv)
 {
